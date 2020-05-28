4d2cc952800c92bf626f107d576f44a33765e226
Zionknight-crypto <info.davidhauser@gmail.com>
2020-05-28 10:13:44 +0300

    Updates

---
+++ precache-manifest.beecee6ff2dbba6621d59ea9df00f01f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa59204031f801a4cf854e7abd78432f",
+    "url": "/Zionknight-crypto.github.io/Robo-Friends/index.html"
+  },
+  {
+    "revision": "fd38df9a20a9ef69fe5b",
+    "url": "/Zionknight-crypto.github.io/Robo-Friends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "6179d3e92dec9cc919df",
+    "url": "/Zionknight-crypto.github.io/Robo-Friends/static/css/main.5fcfc3e8.chunk.css"
+  },
+  {
+    "revision": "fd38df9a20a9ef69fe5b",
+    "url": "/Zionknight-crypto.github.io/Robo-Friends/static/js/2.8a3a465d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Zionknight-crypto.github.io/Robo-Friends/static/js/2.8a3a465d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6179d3e92dec9cc919df",
+    "url": "/Zionknight-crypto.github.io/Robo-Friends/static/js/main.7e4970b3.chunk.js"
+  },
+  {
+    "revision": "56149c2345f9cc11ae2c",
+    "url": "/Zionknight-crypto.github.io/Robo-Friends/static/js/runtime-main.1e4729db.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Zionknight-crypto.github.io/Robo-Friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
